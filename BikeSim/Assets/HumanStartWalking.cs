b141f37517d6cdc640e77c9d9fef82e5385e6961
NycoBartels <75326303+NycoBartels@users.noreply.github.com>
2022-01-19 23:49:38 +0100

    added human walktrigger

---
+++ BikeSim/Assets/HumanStartWalking.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HumanStartWalking : MonoBehaviour
+{
+
+    [SerializeField] private float movespeed = 10f;
+    [SerializeField] private CharacterController player;
+    private Rigidbody rb;
+    private bool isWalking = false;
+
+    void Start()
+    {
+        player = GetComponent<CharacterController>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update() {
+        if (isWalking) {
+            Walk();
+        }
+    }
+
+    private void Walk() {
+        rb.velocity = new Vector3(-movespeed, 0f, 0f);
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if(other.gameObject.tag == "Player") {
+            isWalking = true;
+            Debug.Log("Player detected");
+        }
+    }
+
+}
